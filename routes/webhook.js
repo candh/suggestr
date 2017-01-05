445c3b2bb1e22c4b5a12911c9fb129c00319fbc7
candh <khichihaider@gmail.com>
2017-01-04 21:39:00 -0500

    genre support commit 1.2 - turn the response on

--- routes/webhook.js
+++ routes/webhook.js
@@ -624,7 +624,7 @@ function callSendAPI(messageData) {
             });
         } else {
             console.error("Unable to send message.");
-            //console.error(response);
+            console.error(response);
             console.error(error);
         }
     });
