42e06f1f46bcd09dbbe3d877ce20ed639d3aa0e0
candh <khichihaider@gmail.com>
2017-01-06 21:05:10 -0500

    oppsss my bad

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(req, res) {
+router.get('/', function(req, res) {
     res.send('working good');
 });
 
